6c08282c8d81c9a787706fdfe81192484ac40059
Santiago de Leon <sdeleon28@gmail.com>
2017-10-30 20:00:06 -0300

    Removed obsolete TestUtils mock

--- src/react-native.js
+++ src/react-native.js
@@ -111,7 +111,7 @@ const ReactNativeAddons = {
   // Perf: require('react-addons-perf'), obsolete
   PureRenderMixin: require('react-addons-pure-render-mixin'),
   TestModule: require('./NativeModules/TestModule'),
-  TestUtils: require('react-addons-test-utils'),
+  // TestUtils: require('react-addons-test-utils'), deprecated
   // TODO(lmr): not sure where to find this
   // batchedUpdates: require('ReactUpdates').batchedUpdates, deprecated
   // cloneWithProps: require('react-addons-clone-with-props'), deprecated
