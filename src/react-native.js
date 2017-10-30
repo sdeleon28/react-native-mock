ff5573ddb6de175af12e6aa57512b17dfeb96fdd
Santiago de Leon <sdeleon28@gmail.com>
2017-10-30 14:45:34 -0300

    Refactored components to ES6 syntax

--- src/react-native.js
+++ src/react-native.js
@@ -108,7 +108,7 @@ const ReactNative = {
 // See http://facebook.github.io/react/docs/addons.html
 const ReactNativeAddons = {
   // LinkedStateMixin: require('react-addons-linked-state-mixin') deprecated,
-  Perf: require('react-addons-perf'),
+  // Perf: require('react-addons-perf'), obsolete
   PureRenderMixin: require('react-addons-pure-render-mixin'),
   TestModule: require('./NativeModules/TestModule'),
   TestUtils: require('react-addons-test-utils'),
